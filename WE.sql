c1ebd919bcf40c71d2fe96b076bad0e2a81e0391
WenDaoSanRi <675037440@qq.com>
2017-12-18 18:26:47 +0800

    基金专户

---
+++ WE.sql
@@ -0,0 +1,7 @@
+{'global_audi_method':'proSpecFundWkfService.proSpecFundFlow(model,login)','global_audi_beanId':'proSpecFundWkfService','global_form_action':'pro/proSpecEditWkfView','global_auto_claim_yn':'Y','global_ignoreSelf_yn':'N'}
+
+
+{'global_audi_method':'proSpecFundWkfService.proSpecFundReserveFlow(model,login)','global_audi_beanId':'proSpecFundWkfService','global_form_action':'pro/proSpec/review','global_auto_claim_yn':'Y','global_ignoreSelf_yn':'N'}
+
+
+{'global_audi_method':'proSpecFundWkfService.proLimitControl(model,login)','global_audi_beanId':'proSpecFundWkfService','global_form_action':'pro/proSpec/proSpecLimitControlView','global_auto_claim_yn':'Y','global_ignoreSelf_yn':'N'}
